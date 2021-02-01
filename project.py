8249a587e3306785f89afb61db4cc37e339b75e2
Lev <lev-740@yandex.ru>
2021-02-01 20:13:49 +0300

    всё готово, осталось только украсить

--- project.py
+++ project.py
@@ -4,6 +4,21 @@ import sys
 from math import cos, sin, radians
 
 
+def draw_word(word, flag):
+    font = pygame.font.Font(None, 50)
+    text = font.render(str(word), True, (100, 255, 100))
+    text_w = text.get_width()
+    text_h = text.get_height()
+    if flag == 1:
+        x, y = 11, 11
+    elif flag == 2:
+        x, y = width // 2 - text.get_width() // 2, height // 2 - text.get_height() // 2
+    else:
+        x, y = width - text_w - 11, 11
+    screen.blit(text, (x, y))
+    pygame.draw.rect(screen, (0, 255, 0), (x - 10, y - 10, text_w + 20, text_h + 20), 1)
+
+
 def load_image(name, w, h):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
@@ -12,6 +27,38 @@ def load_image(name, w, h):
     image = pygame.image.load(fullname)
     image = pygame.transform.scale(image, (w, h))
     return image
+
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, file_name, angle, *group):
+        super().__init__(*group)
+        self.width = 20
+        self.height = 10
+        self.image = load_image(file_name, self.width, self.height)
+        self.image.set_colorkey(-1)
+        self.rect = self.image.get_rect()
+        self.angle = angle
+        self.rect.x = pos_x  # + self.width * cos(radians(self.angle))
+        self.rect.y = pos_y  # + self.height * sin(radians(self.angle))
+        self.image = pygame.transform.rotate(self.image, self.angle + 180)
+        self.image.set_colorkey(-1)
+        self.ax = 70 * cos(radians(self.angle))
+        self.ay = 70 * -sin(radians(self.angle))
+        self.timer = 0
+
+    def moving(self):
+        if self.rect.x > width - self.width // 2:
+            self.rect.x = 0 - self.width // 2
+        elif self.rect.x < -self.width // 2:
+            self.rect.x = width - self.width // 2
+        if self.rect.y > height - self.height // 2:
+            self.rect.y = 0 - self.height // 2
+        elif self.rect.y < -self.height // 2:
+            self.rect.y = height - self.height // 2
+        self.timer += 1
+        if self.timer >= FPS * 0.5:
+            self.kill()
+        self.rect = self.rect.move(self.ax, self.ay)
 
 
 class Ship(pygame.sprite.Sprite):
@@ -30,15 +77,15 @@ class Ship(pygame.sprite.Sprite):
         self.ax = 0
         self.ay = 0
         self.angle = 0
+        self.hp = 500
+
 
     def reverse(self, angle):
         self.angle += angle
         if self.angle == 360 or self.angle == -360:
             self.angle = 0
-        print(self.angle)
         self.image = pygame.transform.rotate(self.ship_image, self.angle)
         self.image.set_colorkey(-1)
-        #self.image = pygame.transform.scale(self.ship_image, (self.ship_width, self.ship_height))
 
     def update(self):
         self.ax += 1 * cos(radians(self.angle))
@@ -61,6 +108,12 @@ class Ship(pygame.sprite.Sprite):
             self.rect.y = height - self.ship_height // 2
         self.rect = self.rect.move(self.x, self.y)
 
+    def laser_shot(self, file_name, *group):
+        pos_x = self.rect.x + self.ship_width / 2
+        pos_y = self.rect.y + self.ship_height / 2
+        Laser(pos_x, pos_y, file_name, self.angle, *group)
+        self.hp -= 1
+
 
 pygame.init()
 FPS = 24
@@ -68,34 +121,62 @@ size = width, height = 1200, 900
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
-first_ship = Ship(50, height / 2, 'ship_1.png', all_sprites)
-second_ship = Ship(width - 50, height / 2, 'ship_2.png', all_sprites)
+all_ships = pygame.sprite.Group()
+lasers = pygame.sprite.Group()
+first_ship = Ship(50, height / 2, 'ship_1.png', all_sprites, all_ships)
+second_ship = Ship(width - 50, height / 2, 'ship_2.png', all_sprites, all_ships)
+
 second_ship.reverse(180)
 all_sprites.add(first_ship)
 
 pygame.display.flip()
 
 running = True
-
+FLAG = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if pygame.key.get_pressed()[pygame.K_w]:
-        first_ship.update()
-    if pygame.key.get_pressed()[pygame.K_a]:
-        first_ship.reverse(10)
-    if pygame.key.get_pressed()[pygame.K_d]:
-        first_ship.reverse(-10)
-    if pygame.key.get_pressed()[pygame.K_UP]:
-        second_ship.update()
-    if pygame.key.get_pressed()[pygame.K_LEFT]:
-        second_ship.reverse(10)
-    if pygame.key.get_pressed()[pygame.K_RIGHT]:
-        second_ship.reverse(-10)
-    screen.fill(pygame.Color('white'))
-    first_ship.moving()
-    second_ship.moving()
+    if FLAG:
+        if pygame.key.get_pressed()[pygame.K_w]:
+            first_ship.update()
+        if pygame.key.get_pressed()[pygame.K_a]:
+            first_ship.reverse(10)
+        if pygame.key.get_pressed()[pygame.K_d]:
+            first_ship.reverse(-10)
+        if pygame.key.get_pressed()[pygame.K_x]:
+            first_ship.laser_shot('laser3.png', all_sprites, lasers)
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            second_ship.laser_shot('laser4.png', all_sprites, lasers)
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            second_ship.update()
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            second_ship.reverse(10)
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            second_ship.reverse(-10)
+        screen.fill(pygame.Color('white'))
+        for sprite in all_sprites:
+            sprite.moving()
+    for ship in all_ships:
+        if pygame.sprite.spritecollideany(ship, lasers, collided=None):
+            pygame.sprite.spritecollideany(ship, lasers, collided=None).kill()
+            ship.hp -= 20
+        if ship.hp <= 0:
+            ship.kill()
+            FPS = 10
+    draw_word(first_ship.hp, True)
+    draw_word(second_ship.hp, False)
+    #first_ship.moving()
+    #second_ship.moving()
+    if not first_ship.alive() and not second_ship.alive():
+        draw_word('Ничья', 2)
+        FLAG = False
+    elif not first_ship.alive():
+        draw_word('Победа 2 игрока', 2)
+        FLAG = False
+    elif not second_ship.alive():
+        draw_word('Победа 1 игрока', 2)
+        FLAG = False
     all_sprites.draw(screen)
     pygame.display.flip()
     clock.tick(FPS)
